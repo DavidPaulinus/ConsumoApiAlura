eb40967f46862391e344c2675304c5cedefa197f
DavidPaulinus <davipm@hotmail.com>
2023-04-11 09:42:14 -0300

    transformando dados com biblioteca externa

--- src/br/com/alura/screenmatch/principal/busca/Busca.java
+++ src/br/com/alura/screenmatch/principal/busca/Busca.java
@@ -6,16 +6,33 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Scanner;
+
+import com.google.gson.Gson;
+
+import br.com.alura.screenmatch.modelos.Titulo;
 
 public class Busca {
 	public static void main(String[] args) throws IOException, InterruptedException {
-		HttpClient client = HttpClient.newHttpClient();
-		HttpRequest request = HttpRequest.newBuilder()
-								.uri(URI.create("https://www.omdbapi.com/?t=top+gun&apikey=db0c4f7"))
-								.build();
+		try(Scanner sc = new Scanner(System.in)){
+			System.out.print("Nome do filme: ");
+			var nome = sc.nextLine();
+			
+			var endereco = "https://www.omdbapi.com/?t=" + nome + "&apikey=db0c4f7";
+			
+			HttpClient client = HttpClient.newHttpClient();
+			HttpRequest request = HttpRequest.newBuilder()
+					.uri(URI.create(endereco))
+					.build();
+			
+			HttpResponse<String> response = client.send(request, BodyHandlers.ofString()); 
+			var jj = response.body();
+			System.out.println(jj);
+			
+			Gson gg = new Gson();
+			Titulo tt = gg.fromJson(jj, Titulo.class);
+			System.out.println(tt.getNome());
+		}
 		
-		HttpResponse<String> response = client.send(request, BodyHandlers.ofString()); 
-		
-		System.out.println(response.body());
 	}
 }
